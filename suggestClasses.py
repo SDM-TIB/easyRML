c4781d0896a347c377f04ee772f0a8433671ec64
samiscoding <samaneh.jozashoori@gmail.com>
2021-02-05 14:28:46 +0330

    backend implementation of ontology class and properties are done!
    backend implementation of ontology class and properties are done!

--- suggestClasses.py
+++ suggestClasses.py
@@ -6,13 +6,14 @@ import pandas as pd
 import sys
 import os
 from SPARQLWrapper import SPARQLWrapper,JSON
-import rdflib
 import json
+from rdflib import Graph
+
 
 #################################################################################
 
-def readOntology(endpoint):
-
+#### ontology as an endpoint
+def readOntoEndpoint(endpoint):
     sparql = SPARQLWrapper(endpoint)
     ## retrieve classes from the ontology
     sparql.setQuery("""SELECT distinct ?class 
@@ -23,25 +24,36 @@ def readOntology(endpoint):
     classes = sparql.query().convert()  
     return classes
 
-def readTurtle():
-
-    file = "/mnt/e/GitHub/CLARIFYUnifiedSchema/clarify_v2.ttl"
-    ontologyGraph = rdflib.graph()
-    ontologyGraph.load(file, format='ttl')
-    print (ontology)
+#### ontology as an turtle file
+def readOntoTurtle(filename):
+    file = filename
+    ontologyGraph = Graph()
+    #ontologyGraph = rdflib.graph()
+    ontologyGraph.parse(file, format='ttl')
     query = """SELECT distinct ?class 
         WHERE { 
         ?class <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> 
         <http://www.w3.org/2002/07/owl#Class>. }"""
-    ontoGraph.query(query)
-    for stmt in ontoGraph:
-        print (stmt)
+    queryResult = ontologyGraph.query(query)
+    owlClassList = []
+    for c in queryResult:
+        owlClass = str(c.asdict().values()).split("(")[2].replace(")])","").replace("\'","")
+        owlClassList.append(owlClass)
+    return owlClassList
+
 
 def handler():
+
+    #### ontology as an endpoint
     #user_input = input("Enter the ontology endpoint: ")
     #classes = readOntology(user_input)
-    readTurtle()
     #print (json.dumps(classes,indent=2))
+
+    #### ontology as an turtle file
+    owlClassList = readOntoTurtle("/Users/sam/Documents/GitHub/CLARIFYUnifiedSchema/clarify_v2.ttl")
+    owlClassSeries = pd.Series(owlClassList, name="Classes")
+    owlClassSeries.to_csv("/Users/sam/Documents/GitHub/easyRML/output/classSuggestionOutput.csv",sep=",")
+
 
 if __name__ == "__main__":
         handler()
--- suggestProperties.py
+++ suggestProperties.py
@@ -7,10 +7,12 @@ import sys
 import os
 from SPARQLWrapper import SPARQLWrapper,JSON
 import json
+from rdflib import Graph
+
 
 #################################################################################
 
-def readOntology(endpoint):
+def readOntoEndpoint(endpoint):
 
     sparql = SPARQLWrapper(endpoint)
       
@@ -26,10 +28,38 @@ def readOntology(endpoint):
     properties = sparql.query().convert()
     return properties
 
+#### ontology as an turtle file
+def readOntoTurtle(filename):
+    file = filename
+    ontologyGraph = Graph()
+    #ontologyGraph = rdflib.graph()
+    ontologyGraph.parse(file, format='ttl')
+    query = """SELECT distinct ?protperty 
+        WHERE {{ 
+        ?protperty <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> 
+        <http://www.w3.org/2002/07/owl#ObjectProperty>. }
+        UNION { 
+        ?protperty <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> 
+        <http://www.w3.org/2002/07/owl#DatatypeProperty>. }}"""
+    queryResult = ontologyGraph.query(query)
+    owlPropertyList = []
+    for c in queryResult:
+        owlProperty = str(c.asdict().values()).split("(")[2].replace(")])","").replace("\'","")
+        owlPropertyList.append(owlProperty)
+    return owlPropertyList
+
 def handler():
-    user_input = input("Enter the ontology endpoint: ")
-    properties = readOntology(user_input)
-    print (json.dumps(properties,indent=2))
+
+    #### ontology as an endpoint
+    #user_input = input("Enter the ontology endpoint: ")
+    #properties = readOntology(user_input)
+    #print (json.dumps(properties,indent=2))
+
+    #### ontology as an turtle file
+    owlPropertyList = readOntoTurtle("/Users/sam/Documents/GitHub/CLARIFYUnifiedSchema/clarify_v2.ttl")
+    owlPropertySeries = pd.Series(owlPropertyList, name="Properties")
+    owlPropertySeries.to_csv("/Users/sam/Documents/GitHub/easyRML/output/propertySuggestionOutput.csv",sep=",")
+
 
 if __name__ == "__main__":
         handler()
