638396d2788082a8685a74fdd61269eaa365e555
samiscoding <samaneh.jozashoori@gmail.com>
2021-02-05 12:11:12 +0330

    test ttl file!
    test ttl file!

--- suggestClasses.py
+++ suggestClasses.py
@@ -6,6 +6,7 @@ import pandas as pd
 import sys
 import os
 from SPARQLWrapper import SPARQLWrapper,JSON
+import rdflib
 import json
 
 #################################################################################
@@ -22,10 +23,23 @@ def readOntology(endpoint):
     classes = sparql.query().convert()  
     return classes
 
+def readTurtle(filename):
+
+    ontoGraph = rdflib.graph()
+    ontology = ontoGraph.parse(filename, format="ttl")
+    query = """SELECT distinct ?class 
+        WHERE { 
+        ?class <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> 
+        <http://www.w3.org/2002/07/owl#Class>. }"""
+    ontoGraph.query(query)
+    for stmt in ontoGraph:
+        print (stmt)
+
 def handler():
     user_input = input("Enter the ontology endpoint: ")
-    classes = readOntology(user_input)
-    print (json.dumps(classes,indent=2))
+    #classes = readOntology(user_input)
+    classes = readTurtle(filename)
+    #print (json.dumps(classes,indent=2))
 
 if __name__ == "__main__":
         handler()
