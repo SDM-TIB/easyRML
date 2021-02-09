7fe2d645fb90355fc6cbed2ade216c4bae65f5f8
samiscoding <samaneh.jozashoori@gmail.com>
2021-02-09 20:49:51 +0330

    Update MappingGenerator.py

--- MappingGenerator.py
+++ MappingGenerator.py
@@ -81,31 +81,14 @@ def readConfig(config):
         TM = TM + FM                
     return prefixList,TM
 
-def readOntology(endpoint):
-
-    if str(endpoint).lower() not "":
-        sparql = SPARQLWrapper(str(endpoint).lower())
-        sparql.setQuery("""PREFIX owl: <http://www.w3.org/2002/07/owl#>
-                    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
-                    SELECT ?s 
-                    WHERE { ?s rdf:type owl:ObjectProperty. }""")
-        predicates = sparql.query()
-
-    else:
-        return ("")
-
 
 def handler():
     user_input = input("Enter the path of your file: ")
-    print (user_input)
     config = ConfigParser(interpolation=ExtendedInterpolation())
     config.read(user_input)
-    ? = readOntology(config['main']['ontology_endpoint'])
-    print (?)
     p = config['main']['number_of_prefixes']
     prefixList,TM = readConfig(config)
     mappingFileName = str(outputPath) + "/" + str(config['main']['mapping_file_name']) + ".ttl"
-    print(mappingFileName)
     mappingFile = open(mappingFileName, "w")
     for p in prefixList:
         mappingFile.write(p+"\n")
