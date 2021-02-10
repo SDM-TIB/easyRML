3af1a3a0cbe1e31d6064430c886bf5d6ec78283c
samiscoding <samaneh.jozashoori@gmail.com>
2021-02-10 12:21:52 +0330

    Update app.py

--- app.py
+++ app.py
@@ -11,19 +11,6 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 responseConfig = {}
 
-from flask import Flask, render_template, request, Response, send_file, render_template, send_from_directory
-from flask.json import jsonify
-import json
-import MappingGenerator
-import suggestClasses
-from configparser import ConfigParser, ExtendedInterpolation
-###from werkzeug.utils import secure_filename
-
-UPLOAD_FOLDER = './output'
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-responseConfig = {}
-
 
 @app.route('/', methods=['GET'])
 def index():
@@ -87,65 +74,3 @@ def api_mappings():
 
 if __name__ == "__main__":
     app.run(port=5502, host="0.0.0.0")
-
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
-
-
-@app.route('/api/readOnto', methods=['POST'])
-def api_readOnto():
-    try:
-        file = request.files['file']
-        class_list = suggestClasses(file)
-        print (class_list)
-        class_json = json.dumps(class_list)
-
-    except KeyError:
-        print('request args/form exception ... ', request.args, request.form)
-        return Response(json.dumps({}),  mimetype="application/json")
-
-    return Response(class_json,  mimetype="application/json") 
-   
-
-@app.route('/api/verifying', methods=['POST'])
-def api_verifying():
-
-    print('request') 
-    
-
-
-@app.route('/api/mappings', methods=['POST'])
-def api_mappings():
-    try:
-        user_input = request.get_data().decode("utf-8")
-        config = ConfigParser(interpolation=ExtendedInterpolation())
-        config.read_string(user_input)
-        outputPath = config['main']['output_folder']
-        prefixList,TM = MappingGenerator.readConfig(config)
-        mappingFileName = str(outputPath) + str(config['main']['mapping_file_name']) + ".ttl"
-        mappingFile = open(mappingFileName, "w")
-        for p in prefixList:
-            mappingFile.write(p+"\n")
-        mappingFile.write(TM)
-        mappingFile.close()
-        print(mappingFileName)
-        print(prefixList,TM)
-     
-    except KeyError:
-        print('request args/form exception ... ', request.args, request.form)
-        return Response(json.dumps({}),  mimetype="application/json")
-    
-    prefixes = ''
-    for prefix in prefixList:
-        prefixes += ''.join(prefix) + '\n'
-    responseConfig['answer'] = prefixes + TM
-   
-    return Response(prefixes + TM, mimetype='text/plain')
-
-
-    return Response(json.dumps({}),  mimetype="application/json")
-    
-
-if __name__ == "__main__":
-    app.run(port=5501, host="0.0.0.0")
