bf01864ff631164f816c94e17be1bf7823b5f477
Akbari <Ahbi.Akbari@tib.eu>
2021-05-10 12:13:11 +0200

    ajax code in separate js file

---
+++ static/js/readJson.js
@@ -0,0 +1,11 @@
+$.ajax({
+    url:'/suggestPrefix',
+    type:'GET',
+    contentType: "application/json",
+    dataType: 'json',
+      success: function( json ) {
+        $.each(json, function(file) {
+            $('#inputGroupSelect02').append($('<option value="' + this + '">' + this + '</option>'));
+          });
+        }
+      });
