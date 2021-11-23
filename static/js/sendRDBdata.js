8b29c7bc8ae913cf182873397f4bce35afcd1821
abhi055 <abhiakbari023@gmail.com>
2021-11-23 12:41:26 +0100

    rdb data done

--- static/js/sendRDBdata.js
+++ static/js/sendRDBdata.js
@@ -9,6 +9,7 @@ $(document).ready(function () {
         var databaseusername = $("#databaseusername").val();
         var databasetable = $("#databasetable").val();
         var databasequery = $("#databasequery").val();
+        var databasequeryversion = $("#databaseqeryversion").val();
         var databasehost = $("#databasehost").val();
         var databaseport = $("#databaseport").val();
 
@@ -21,12 +22,13 @@ $(document).ready(function () {
             databaseusername: databaseusername,
             databasetable: databasetable,
             databasequery: databasequery,
+            sqlqueryversion: databasequeryversion,
             RDB_host: databasehost,
             RDB_port: databaseport
         });
 
-        // var finalRDBdata = JSON.stringify(RDBdata);
-        // console.log(finalRDBdata);
+        var finalRDBdata = JSON.stringify(RDBdata);
+        console.log(finalRDBdata);
 
 
         $.ajax({
@@ -37,6 +39,41 @@ $(document).ready(function () {
             contentType: "application/json; charset=utf-8",
             success: function (data) {
                 alert(data);
+
+
+                $('#selectdatafield').empty();
+                $('#selectdatafield').append($('<option>', { value: "default", text: "--select the data field--" }).prop('disabled', true));
+                $("#selectdatafield").trigger("chosen:updated");
+
+                $('#selectdatafieldsuggestion').empty();
+                $('#selectdatafieldsuggestion').append($('<option>', { value: "default", text: "--select the data field--" }).prop('selected', true));
+
+                $('#predicateselectdatafield').empty();
+                $('#predicateselectdatafield').append($('<option>', { value: "default", text: "--select the data field--" }).prop('disabled', true));
+                $("#predicateselectdatafield").trigger("chosen:updated");
+
+                $('#joinconditionchild').empty();
+                $('#joinconditionchild').append($('<option>', { value: "default", text: "--joinCondition child--" }).prop('selected', true));
+
+                $.ajax({
+                    url: '/receiveDataFields_RDB',
+                    type: 'GET',
+                    contentType: "application/json",
+                    dataType: 'json',
+                    success: function (json) {
+                        $.each(json, function (file) {
+                            $('#selectdatafield').append($('<option value="' + this + '">' + this + '</option>'));
+                            $("#selectdatafield").trigger("chosen:updated");
+
+                            $('#selectdatafieldsuggestion').append($('<option value="' + this + '">' + this + '</option>'));
+
+                            $('#predicateselectdatafield').append($('<option value="' + this + '">' + this + '</option>'));
+                            $("#predicateselectdatafield").trigger("chosen:updated");
+
+                            $('#joinconditionchild').append($('<option value="' + this + '">' + this + '</option>'));
+                        });
+                    }
+                });
             },
             error: function (jqXHR, textStatus, errorThrown) {
                 console.log(jqXHR);
