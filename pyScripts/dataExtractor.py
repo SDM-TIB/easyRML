4a4f05fe15a7cf4d8e184f94d020bbfe678ff69c
Sam J <samaneh.jozashoori@gmail.com>
2021-11-29 11:16:59 +0100

    Update dataExtractor.py

--- pyScripts/dataExtractor.py
+++ pyScripts/dataExtractor.py
@@ -98,13 +98,13 @@ def extractFields_csv(file):
 def extractFields_RDB(file):
  
     mydb = mysql.connector.connect(
-          host=file["RDB_host"],
-          port=file["RDB_port"],
-          user=file["databaseusername"],
-          passwd=file["databasepassword"],
-          database=file["databasename"] 
+          host=file[RDBdata][0]["RDB_host"],
+          port=file[RDBdata][0]["RDB_port"],
+          user=file[RDBdata][0]["databaseusername"],
+          passwd=file[RDBdata][0]["databasepassword"],
+          database=file[RDBdata][0]["databasename"] 
         )
-    table = file["databasetable"]
+    table = file[RDBdata][0]["databasetable"]
     query="""SELECT COLUMN_NAME
     FROM INFORMATION_SCHEMA.COLUMNS
     WHERE TABLE_NAME ='""" + table + """'"""
