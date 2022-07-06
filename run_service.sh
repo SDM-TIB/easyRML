44aa69db92c8622333a6287d57136eefb4283bb9
Samaneh Jozashoori <samaneh.jozashoori@gmail.com>
2022-07-06 10:32:09 +0200

    Update run_service.sh

--- run_service.sh
+++ run_service.sh
@@ -2,6 +2,6 @@
 
 echo "***** Running the service on port 5000 ***** "
 # gunicorn3 -w 4 -b 0.0.0.0:5003 --timeout 9000 iasis_lc_exploration_service:app
-
+cd ./app/
 npm run start
 python3 ./pyScripts/app.py
