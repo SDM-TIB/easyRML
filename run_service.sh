2e098d9fa675678bca155a825ca4996a2390dc07
Sam J <samaneh.jozashoori@gmail.com>
2022-07-07 19:42:36 +0200

    run script is updated!

--- run_service.sh
+++ run_service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "***** Running the service on port 4200 ***** "
+echo "***** Running the service on port 5000 ***** "
 # gunicorn3 -w 4 -b 0.0.0.0:5003 --timeout 9000 iasis_lc_exploration_service:app
-python3 ../pyScripts/app.py
+python3 ./pyScripts/app.py
