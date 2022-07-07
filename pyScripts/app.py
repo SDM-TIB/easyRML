4fbf6305b3ea2f409c41fa53b87dbc46367730ca
Sam J <samaneh.jozashoori@gmail.com>
2022-07-07 19:54:38 +0200

    Update app.py

--- pyScripts/app.py
+++ pyScripts/app.py
@@ -190,5 +190,5 @@ def generateMapping():
 ############################################
 
 if __name__ == "__main__":
-    app.run(port=5025, host="0.0.0.0")
+    app.run(port=5000, host="0.0.0.0")
 
